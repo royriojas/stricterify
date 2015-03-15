50eb5f3f0033cc702e5d7a07e065b77d74f482f7
Roy Riojas <roy@kno.com>
2015-03-15 04:01:54 -0700

    BUG: Fix false positives for async tests

--- specs/index.js
+++ specs/index.js
@@ -3,7 +3,7 @@ describe( 'stricterify', function () {
   var transform = require( '../' );
   var path = require( 'path' );
 
-  it( 'should append `use strict` to modules that do not have it', function () {
+  it( 'should append `use strict` to modules that do not have it', function ( done ) {
 
     var dummyJsFile = path.resolve( __dirname, '../testFixtures/testWithConfig/dummy.js' );
 
@@ -13,13 +13,15 @@ describe( 'stricterify', function () {
       content: content
     }, function ( err, transformed ) {
       if ( !err ) {
-        expect( transformed ).to.equal( '\n\n"use strict";\n\nvar fn = function () {};\nmodule.exports = fn;' );
+
+        expect( transformed ).to.be.equal( '\n\n"use strict";\n\nvar fn = function () {};\nmodule.exports = fn;' );
+        done();
       }
       throw err;
     }
     );
   } );
-  it( 'should skip adding it if already have `use strict`', function () {
+  it( 'should skip adding it if already have `use strict`', function ( done ) {
 
     var dummyJsFile = path.resolve( __dirname, '../testFixtures/testWithConfig/dummy.js' );
 
@@ -29,7 +31,9 @@ describe( 'stricterify', function () {
       content: content
     }, function ( err, transformed ) {
       if ( !err ) {
-        expect( transformed ).to.equal( '"use strict";\nvar fn = function () {};\nmodule.exports = fn;' );
+
+        expect( transformed ).to.be.equal( '"use strict";\nvar fn = function () {};\nmodule.exports = fn;' );
+        done();
       }
       throw err;
     }
