857769c427261eb34eb002fea87817982fcf54ed
royriojas <roy@kno.com>
2015-07-17 17:58:39 -0700

    ENH: Use the functionality provided by `browserify-transform-tools`

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
 var transformTools = require( 'browserify-transform-tools' );
-var path = require( 'path' );
 
 var options = {
   excludeExtensions: [
     '.json'
+  ],
+  includeExtensions: [
+    '.jsx',
+    '.js',
+    '.es6'
   ]
 };
 
 module.exports = transformTools.makeStringTransform( 'stricterify', options, function ( content, transformOptions, done ) {
-  var file = transformOptions.file;
-  var extension = path.extname( file );
-
-  var configData = transformOptions.configData || {};
-  var config = configData.config || {};
-
-  if ( config.checkIfSkip ) {
-    if ( config.checkIfSkip( file ) ) {
-      done( null, content );
-    }
-  }
-
-  if ( content.indexOf( 'use strict' ) === -1 && (extension === '.js' || extension === '.jsx') ) {
+  if ( content.indexOf( 'use strict' ) === -1 ) {
     content = '\n\n"use strict";\n\n' + content;
   }
   done( null, content );
--- specs/index.js
+++ specs/index.js
@@ -40,45 +40,4 @@ describe( 'stricterify', function () {
 
   } );
 
-  it( 'should skip adding `use strict` if configured to skip it', function ( done ) {
-    var dummyJsFile = path.resolve( __dirname, '../testFixtures/testWithConfig/dummy.js' );
-
-    var content = 'var fn = function () {};\nmodule.exports = fn;';
-
-    transformTools.runTransform( transform.configure( {
-      checkIfSkip: function ( file ) {
-        return !!( file.match( /dummy\.js/ ));
-      }
-    } ), dummyJsFile, {
-      content: content
-    }, function ( err, transformed ) {
-      if ( !err ) {
-
-        expect( transformed ).to.be.equal( 'var fn = function () {};\nmodule.exports = fn;' );
-        done();
-      }
-      throw err;
-    } );
-  } );
-
-  it( 'should not skip adding it if the checkIfSkip function returns false', function ( done ) {
-    var dummyJsFile = path.resolve( __dirname, '../testFixtures/testWithConfig/dummy.js' );
-
-    var content = 'var fn = function () {};\nmodule.exports = fn;';
-
-    transformTools.runTransform( transform.configure( {
-      checkIfSkip: function ( file ) {
-        return !!( file.match( /funnyname\.js/ ));
-      }
-    } ), dummyJsFile, {
-      content: content
-    }, function ( err, transformed ) {
-      if ( !err ) {
-
-        expect( transformed ).to.be.equal( '\n\n"use strict";\n\nvar fn = function () {};\nmodule.exports = fn;' );
-        done();
-      }
-      throw err;
-    } );
-  } );
 } );
